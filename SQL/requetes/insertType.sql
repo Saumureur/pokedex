de18f3a6a01244f2ef1750b8f2c075badb1e35d2
Saumureur <dorian.levignat@gmail.com>
2023-03-09 10:39:33 +0100

    modification iD

--- SQL/requetes/insertType.sql
+++ SQL/requetes/insertType.sql
@@ -1,9 +1,10 @@
 INSERT INTO Type (typeNom, typelogoURL) VALUES 
 (0,'Normal', 'https://www.pokepedia.fr/images/b/bf/Miniature_Type_Normal_EV.png'),
-(1,'Feu', 'https://www.pokepedia.fr/images/c/c7/Miniature_Type_Feu_EV.png'),
-(2,'Eau', 'https://www.pokepedia.fr/images/3/3d/Miniature_Type_Eau_EV.png'),
-(3,'Plante', 'https://www.pokepedia.fr/images/d/d9/Miniature_Type_Plante_EV.png'),
-(4,'Electrik', 'https://www.pokepedia.fr/images/6/6d/Miniature_Type_%C3%89lectrik_EV.png'),
-(5,'Vol', 'https://www.pokepedia.fr/images/9/99/Miniature_Type_Vol_EV.png'),
-(6,'Sol', 'https://www.pokepedia.fr/images/4/40/Miniature_Type_Sol_EV.png'),
-(7,'Combat', 'https://www.pokepedia.fr/images/9/96/Miniature_Type_Combat_EV.png');
+(10,'Feu', 'https://www.pokepedia.fr/images/c/c7/Miniature_Type_Feu_EV.png'),
+(11,'Eau', 'https://www.pokepedia.fr/images/3/3d/Miniature_Type_Eau_EV.png'),
+(12,'Plante', 'https://www.pokepedia.fr/images/d/d9/Miniature_Type_Plante_EV.png'),
+(13,'Electrik', 'https://www.pokepedia.fr/images/6/6d/Miniature_Type_%C3%89lectrik_EV.png'),
+(3,'Vol', 'https://www.pokepedia.fr/images/9/99/Miniature_Type_Vol_EV.png'),
+(5,'Sol', 'https://www.pokepedia.fr/images/4/40/Miniature_Type_Sol_EV.png'),
+(2,'Combat', 'https://www.pokepedia.fr/images/9/96/Miniature_Type_Combat_EV.png'),
+(14,'Psy', '/images/thumb/8/81/Miniature_Type_Psy_EV.png/80px-Miniature_Type_Psy_EV.png');
