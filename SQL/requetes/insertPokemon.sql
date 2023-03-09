f164ce1af214b150917d0c857a5dc66b1b761d24
Saumureur <dorian.levignat@gmail.com>
2023-03-09 08:06:20 +0100

    add pokemon

---
+++ SQL/requetes/insertPokemon.sql
@@ -0,0 +1,6 @@
+INSERT INTO Pokemon (pokemonID, pokemonName,pokemonDescription,pokemonPreviousEvolution,pokemonFollowingEvolution,pokemonType1, pokemonType2) VALUES
+(60, "Ptitard", "Ptitard (anglais : Poliwag ; japonais : ニョロモ Nyoromo) est un Pokémon de type Eau de la première génération. ", NULL, 61,2,NULL),
+(61, "Têtarte", "Têtarte (anglais : Poliwhirl ; japonais : ニョロゾ Nyorozo) est un Pokémon de type Eau de la première génération. ", 60, 62,2,NULL)
+(60, "Ptitard", "Ptitard (anglais : Poliwag ; japonais : ニョロモ Nyoromo) est un Pokémon de type Eau de la première génération. ", 62, NULL,2,7),
+(58, "Caninos", "Caninos (anglais : Growlithe ; japonais : ガーディ Gardie[1]) est un Pokémon chien de type Feu de la première génération. ", NULL, 59,1,NULL),
+(59, "Arcanin", "Arcanin (anglais : Arcanine ; japonais : ウインディ Windie[1]) est un Pokémon de type Feu de la première génération. Il est parfois à tort considéré comme légendaire, du fait du nom de sa famille pouvant prêter à confusion. ", 58, NULL,1,NULL),
