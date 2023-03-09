dda34767128627e26edb7dc71e8fc5f6397b639e
Saumureur <93099074+Saumureur@users.noreply.github.com>
2023-03-09 09:57:22 +0100

    Update insertPokemon.sql

--- SQL/requetes/insertPokemon.sql
+++ SQL/requetes/insertPokemon.sql
@@ -4,3 +4,11 @@ INSERT INTO Pokemon (pokemonID, pokemonName,pokemonDescription,pokemonPreviousEv
 (60, "Ptitard", "Ptitard (anglais : Poliwag ; japonais : ニョロモ Nyoromo) est un Pokémon de type Eau de la première génération. ", 62, NULL,2,7),
 (58, "Caninos", "Caninos (anglais : Growlithe ; japonais : ガーディ Gardie[1]) est un Pokémon chien de type Feu de la première génération. ", NULL, 59,1,NULL),
 (59, "Arcanin", "Arcanin (anglais : Arcanine ; japonais : ウインディ Windie[1]) est un Pokémon de type Feu de la première génération. Il est parfois à tort considéré comme légendaire, du fait du nom de sa famille pouvant prêter à confusion. ", 58, NULL,1,NULL),
+(63, "Abra", "Abra (anglais : Abra ; japonais : ケーシィ Casey) est un Pokémon de type Psy de la première génération. ", NULL, 64, 14, NULL),
+(64, "Kadabra", "Kadabra (anglais : Kadabra ; japonais : ユンゲラー Yungerer) est un Pokémon de type Psy de la première génération. ", 63, 65, 14, NULL),
+(65, "Alakazam", "Alakazam (anglais : Alakazam ; japonais : フーディン Foodin) est un Pokémon de type Psy de la première génération. ", 64, NULL, 14, NULL),
+(66, "Machoc", "Machoc (anglais : Machop ; japonais : ワンリキー Wanrikii) est un Pokémon de type Combat de la première génération. ", NULL, 67, 5, NULL),
+(67, "Machopeur", "Machopeur (anglais : Machoke ; japonais : ゴーリキー Goriky) est un Pokémon de type Combat de la première génération. ", 66, 68, 5, NULL),
+(68, "Mackogneur", "Mackogneur (anglais : Machamp ; japonais : カイリキー Kairiky) est un Pokémon de type Combat de la première génération. ", 67, NULL, 5, NULL),
+(69, "Chétiflor", "Chétiflor (anglais : Bellsprout ; japonais : マダツボミ Madatsubomi) est un Pokémon de type Plante et Poison de la première génération. ", NULL, 70, 12, 4),
+(70, "Empiflor", "Empiflor (anglais : Weepinbell ; japonais : ウツドン Utsudon) est un Pokémon de type Plante et Poison de la première génération. ", 69, NULL, 12, 4);
