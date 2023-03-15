472f1b520f80bc8499152f82612ee8a82184c75b
Saumureur <dorian.levignat@gmail.com>
2023-03-15 11:11:25 +0100

    modif pokemon
    On enleve les caractère chinois

--- SQL/requetes/insertPokemon.sql
+++ SQL/requetes/insertPokemon.sql
@@ -1,14 +1,14 @@
 INSERT INTO Pokemon (`pokemonID`, `pokemonName`,`pokemonDescription`,`pokemonPreviousEvolution`,`pokemonFollowingEvolution`,`pokemonType1`, `pokemonType2`) VALUES
-(60, "Ptitard", "Ptitard (anglais : Poliwag ; japonais : ニョロモ Nyoromo) est un Pokémon de type Eau de la première génération. ", NULL, 61,11,NULL),
-(61, "Têtarte", "Têtarte (anglais : Poliwhirl ; japonais : ニョロゾ Nyorozo) est un Pokémon de type Eau de la première génération. ", 60, 62,11,NULL)
-(62, "Tartard", "Tartard (anglais : Poliwrath ; japonais : ニョロボン Nyorobon) est un Pokémon de type Eau et Combat de la première génération.  ", 62, NULL,11,2),
-(58, "Caninos", "Caninos (anglais : Growlithe ; japonais : ガーディ Gardie[1]) est un Pokémon chien de type Feu de la première génération. ", NULL, 59,10,NULL),
-(59, "Arcanin", "Arcanin (anglais : Arcanine ; japonais : ウインディ Windie[1]) est un Pokémon de type Feu de la première génération. Il est parfois à tort considéré comme légendaire, du fait du nom de sa famille pouvant prêter à confusion. ", 58, NULL,10,NULL),
-(63, "Abra", "Abra (anglais : Abra ; japonais : ケーシィ Casey) est un Pokémon de type Psy de la première génération. ", NULL, 64, 14, NULL),
-(64, "Kadabra", "Kadabra (anglais : Kadabra ; japonais : ユンゲラー Yungerer) est un Pokémon de type Psy de la première génération. ", 63, 65, 14, NULL),
-(65, "Alakazam", "Alakazam (anglais : Alakazam ; japonais : フーディン Foodin) est un Pokémon de type Psy de la première génération. ", 64, NULL, 14, NULL),
-(66, "Machoc", "Machoc (anglais : Machop ; japonais : ワンリキー Wanrikii) est un Pokémon de type Combat de la première génération. ", NULL, 67, 2  , NULL),
-(67, "Machopeur", "Machopeur (anglais : Machoke ; japonais : ゴーリキー Goriky) est un Pokémon de type Combat de la première génération. ", 66, 68, 2, NULL),
-(68, "Mackogneur", "Mackogneur (anglais : Machamp ; japonais : カイリキー Kairiky) est un Pokémon de type Combat de la première génération. ", 67, NULL, 2, NULL),
-(69, "Chétiflor", "Chétiflor (anglais : Bellsprout ; japonais : マダツボミ Madatsubomi) est un Pokémon de type Plante et Poison de la première génération. ", NULL, 70, 12, NULL),
-(70, "Empiflor", "Empiflor (anglais : Weepinbell ; japonais : ウツドン Utsudon) est un Pokémon de type Plante et Poison de la première génération. ", 69, NULL, 12, NULL);
+(60, "Ptitard", "Ptitard (anglais : Poliwag ; japonais :  Nyoromo) est un Pokémon de type Eau de la première génération. ", NULL, 61,11,NULL),
+(61, "Têtarte", "Têtarte (anglais : Poliwhirl ; japonais : Nyorozo) est un Pokémon de type Eau de la première génération. ", 60, 62,11,NULL),
+(62, "Tartard", "Tartard (anglais : Poliwrath ; japonais : Nyorobon) est un Pokémon de type Eau et Combat de la première génération.  ", 62, NULL,11,2),
+(58, "Caninos", "Caninos (anglais : Growlithe ; japonais : Gardie) est un Pokémon chien de type Feu de la première génération. ", NULL, 59,10,NULL),
+(59, "Arcanin", "Arcanin (anglais : Arcanine ; japonais : Windie) est un Pokémon de type Feu de la première génération. Il est parfois à tort considéré comme légendaire, du fait du nom de sa famille pouvant prêter à confusion. ", 58, NULL,10,NULL),
+(63, "Abra", "Abra (anglais : Abra ; japonais : Casey) est un Pokémon de type Psy de la première génération. ", NULL, 64, 14, NULL),
+(64, "Kadabra", "Kadabra (anglais : Kadabra ; japonais : Yungerer) est un Pokémon de type Psy de la première génération. ", 63, 65, 14, NULL),
+(65, "Alakazam", "Alakazam (anglais : Alakazam ; japonais : Foodin) est un Pokémon de type Psy de la première génération. ", 64, NULL, 14, NULL),
+(66, "Machoc", "Machoc (anglais : Machop ; japonais : Wanrikii) est un Pokémon de type Combat de la première génération. ", NULL, 67, 2  , NULL),
+(67, "Machopeur", "Machopeur (anglais : Machoke ; japonais : Goriky) est un Pokémon de type Combat de la première génération. ", 66, 68, 2, NULL),
+(68, "Mackogneur", "Mackogneur (anglais : Machamp ; japonais : Kairiky) est un Pokémon de type Combat de la première génération. ", 67, NULL, 2, NULL),
+(69, "Chétiflor", "Chétiflor (anglais : Bellsprout ; japonais : Madatsubomi) est un Pokémon de type Plante et Poison de la première génération. ", NULL, 70, 12, NULL),
+(70, "Empiflor", "Empiflor (anglais : Weepinbell ; japonais : Utsudon) est un Pokémon de type Plante et Poison de la première génération. ", 69, NULL, 12, NULL);
